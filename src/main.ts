7ce2fcf3a276cfbd2c418add4b17507d58b89722
Besher-Hamze <besherhamze97@gmail.com>
2025-09-06 17:00:32 +0300

    Update CORS configuration to allow access from a new IP address

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://62.171.153.198:3398'],
     credentials: true,
   });
 
